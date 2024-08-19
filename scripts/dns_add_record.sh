8a16525b65b29367ee82b5467865935ec5ca0de4
Rune Van Bouwel Brams <rune.vanbouwelbrams@student.ucll.be>
2024-08-19 14:28:49 +0200

    Working dns_add_record.sh

--- scripts/dns_add_record.sh
+++ scripts/dns_add_record.sh
@@ -1,45 +1,88 @@
 #!/bin/bash
 echo $1
-# Check if it's executed as root
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root."
-    exit
-fi
 
-increment_serial() {
-	zone_file="/etc/bind/zones/db.$1"
-	oldSerial=$(awk '/Serial/ {print $1}' "$zone_file" | tr -d ';')
-	newSerial=$((oldSerial + 1))
-	sed -i "s/\(^[[:space:]]*${oldSerial}[[:space:]]*;\sSerial\)/\t\t\t${newSerial}; Serial/" "$zone_file"
+# Print usage information
+print_usage() {
+    echo "Usage: $0 -t <record_type> <record_name> <record_value> <zone>"
+    echo "  -t: Record type (A, MX, CNAME)"
+    echo "  <record_name>: Name of the record"
+    echo "  <record_value>: Value for the record"
+    echo "  <zone>: Zone file name (e.g., foobar5.rune-vanbouwelbrams.sasm.uclllabs.be)"
+    exit 1
 }
 
-if [ "$1" == "-t" ] && [ "$2" ] && [ "$3" ] && [ "$4" ]; 
-then
-        case "$2" in
-        CNAME)
-		echo "$3        IN         CNAME   $4." >> "/etc/bind/zones/db.$4"
-		increment_serial "$4"
-        ;;
-
-        MX)
-                echo "  IN         MX      10      $3" >> "/etc/bind/zones/db.$5"
-                echo "$3        IN      A       $4" >> "/etc/bind/zones/db.$5"    
-                increment_serial "$5"
-        ;;
-
-        *)
-                echo "$3        IN      A       $4" >> "/etc/bind/zones/db.$5"
-                increment_serial "$5"
-        ;;
-       esac
-    	systemctl restart bind9
-elif [ "$1" ] && [ "$2" ] && [ "$3" ];
-then
-	echo "$1        IN      A       $2" >> "/etc/bind/zones/db.$3"
-	increment_serial "$3"
-	systemctl restart bind9
-else
-    	echo "dns_add_record -t <A | MX | CNAME> <name> <value> <zone>: options cannot be empty"
+# Check if it's executed as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root."
+    exit 1
 fi
 
+# Function to increment the serial number in the zone file
+increment_serial() {
+    zone_file="/etc/bind/zones/db.$1"
+    oldSerial=$(awk '/Serial/ {print $1}' "$zone_file" | tr -d ';')
+    newSerial=$((oldSerial + 1))
+    sed -i "s/\(^[[:space:]]*${oldSerial}[[:space:]]*;\sSerial\)/\t\t\t${newSerial} ; Serial/" "$zone_file"
+}
+
+# Parse options and arguments
+while getopts "t:" opt; do
+    case $opt in
+        t)
+            record_type="$OPTARG"
+            if [[ "$record_type" != "A" && "$record_type" != "MX" && "$record_type" != "CNAME" ]]; then
+                echo "Error: Invalid record type. Supported types are A, MX, and CNAME."
+                print_usage
+            fi
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            print_usage
+            ;;
+    esac
+done
+
+# Shift the processed options
+shift $((OPTIND - 1))
+
+# Ensure exactly 3 arguments are provided
+if [ "$#" -ne 3 ]; then
+    print_usage
+fi
+
+# Get the arguments
+record_name="$1"
+record_value="$2"
+zone="$3"
+
+zone_file="/etc/bind/zones/db.$zone"
+
+# Handle different types of records
+case "$record_type" in
+    A)
+        echo "$record_name.$zone. IN A $record_value" >> "$zone_file"
+        ;;
+    MX)
+        echo "$record_name IN MX 10 $record_value." >> "$zone_file"
+        echo "$record_value. IN A $record_value" >> "$zone_file"
+        ;;
+    CNAME)
+        echo "$record_name.$zone. IN CNAME $record_value" >> "$zone_file"
+        ;;
+    *)
+        echo "Unsupported record type: $record_type"
+        exit 1
+        ;;
+esac
+
+# Increment the serial number
+increment_serial "$zone"
+
+echo "$record_name.$zone. IN A $record_value"
+echo "$record_name IN MX 10 $record_value."
+echo "$record_value. IN A $record_value"
+echo "$record_name.$zone. IN CNAME $record_value"
+
 systemctl restart bind9
+
+echo "Record added: $record_name.$zone IN $record_type $record_value"
