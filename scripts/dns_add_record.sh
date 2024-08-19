a171c64b3e6c4fa1c981ca6a541a7df8aadf227e
Rune Van Bouwel Brams <rune.vanbouwelbrams@student.ucll.be>
2024-08-19 10:21:38 +0200

    Changed dns_add_records.sh

--- scripts/dns_add_record.sh
+++ scripts/dns_add_record.sh
@@ -6,34 +6,40 @@ if [ "$EUID" -ne 0 ]
     exit
 fi
 
+increment_serial() {
+	zone_file="/etc/bind/zones/db.$1"
+	oldSerial=$(awk '/Serial/ {print $1}' "$zone_file" | tr -d ';')
+	newSerial=$((oldSerial + 1))
+	sed -i "s/\(^[[:space:]]*${oldSerial}[[:space:]]*;\sSerial\)/\t\t\t${newSerial}; Serial/" "$zone_file"
+}
+
 if [ "$1" == "-t" ] && [ "$2" ] && [ "$3" ] && [ "$4" ]; 
 then
         case "$2" in
         CNAME)
-                echo "$3        IN         CNAME   $4." >> "/etc/bind/zones/db.$4"
-                oldSerial=$(awk '/Serial/ {print $1}' /etc/bind/zones/db.$4)
-                newSerial=$((oldSerial + 1))
-                sed -i "s/\(^[[:space:]]*${oldSerial}[[:space:]]*;\sSerial\)/\t\t\t${newSerial}; Serial/" /etc/bind/zones/db.$4
+		echo "$3        IN         CNAME   $4." >> "/etc/bind/zones/db.$4"
+		increment_serial "$4"
         ;;
 
         MX)
                 echo "  IN         MX      10      $3" >> "/etc/bind/zones/db.$5"
                 echo "$3        IN      A       $4" >> "/etc/bind/zones/db.$5"    
-            oldSerial=$(awk '/Serial/ {print $1}' /etc/bind/zones/db.$5)
-                newSerial=$((oldSerial + 1))
-                sed -i "s/\(^[[:space:]]*${oldSerial}[[:space:]]*;\sSerial\)/\t\t\t${newSerial}; Serial/" /etc/bind/zones/db.$5
+                increment_serial "$5"
         ;;
 
         *)
                 echo "$3        IN      A       $4" >> "/etc/bind/zones/db.$5"
-                oldSerial=$(awk '/Serial/ {print $1}' /etc/bind/zones/db.$5)
-                newSerial=$((oldSerial + 1))
-                sed -i "s/\(^[[:space:]]*${oldSerial}[[:space:]]*;\sSerial\)/\t\t\t${newSerial}; Serial/" /etc/bind/zones/db.$5
+                increment_serial "$5"
         ;;
        esac
- 
-    systemctl restart bind9
+    	systemctl restart bind9
+elif [ "$1" ] && [ "$2" ] && [ "$3" ];
+then
+	echo "$1        IN      A       $2" >> "/etc/bind/zones/db.$3"
+	increment_serial "$3"
+	systemctl restart bind9
 else
-    echo "dns_add_record -t <A | MX | CNAME> <name> <value> <zone>: options cannot be empty"
+    	echo "dns_add_record -t <A | MX | CNAME> <name> <value> <zone>: options cannot be empty"
 fi
 
+systemctl restart bind9
